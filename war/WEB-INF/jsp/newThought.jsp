fb3e5775159535486a94880d9db40043500b2a00
Aneesh P U <aneeshpu@gmail.com>
2009-11-23 22:05:06 +0530

    thoughts now show time as well

--- war/WEB-INF/jsp/newThought.jsp
+++ war/WEB-INF/jsp/newThought.jsp
@@ -1,4 +1,10 @@
+<%@page import="com.aneesh.gae.domain.QuickThought" %>
+
 <%
 response.setContentType("application/json");
 %>
-{newThought: '<%=request.getAttribute("thought")%>'}
+
+{
+	newThought: '<%=request.getAttribute("thought")%>'
+	,created: '<%= ((QuickThought)request.getAttribute("thought")).when()%>'
+}
--- war/js/quickThought.js
+++ war/js/quickThought.js
@@ -4,6 +4,59 @@ function registerObservers(){
 	$('thoughtCreator').observe('click', createThought);
 }
 
+var Mind = Class.create({
+	
+	show: function(quickThought){
+		this.thoughtDisplayBoard().insert(this.format(quickThought));
+	}
+	
+	,format: function(quickThought){
+		var list=this.list();
+		list.insert(this.content(quickThought));		
+		list.insert(this.when(quickThought));
+		return list;
+	}
+	
+	,when: function(quickThought){
+		var span = new Element('span');
+		span.addClassName("time");
+		span.insert(quickThought.when());
+		return span;
+	}
+	
+	,content: function(quickThought){
+		var span = new Element('span');
+		span.addClassName("thought");
+		span.insert(quickThought.content());
+		return span;
+	}
+	
+	,list: function(){
+		return new Element("<li>");
+	}
+
+	,thoughtDisplayBoard: function(){
+		return $('newThoughts');
+	}
+});
+
+var QuickThought = Class.create({
+	
+	initialize: function(quickThoughtJSONString){
+		this.quickThoughtJSONString = quickThoughtJSONString;
+	}
+
+	,content: function(){
+		return this.quickThoughtJSONString.newThought;
+	}
+	
+	,when: function(){
+		return this.quickThoughtJSONString.created;
+	}
+});
+
+var mindDump = new Mind();
+
 function createThought(event){
 
 	var newThought = $('thoughtBox').getValue();
@@ -12,8 +65,9 @@ function createThought(event){
 		method: 'get',
 		
 		onSuccess: function(transport){
-			var newThought = transport.responseText;
-			$('newThoughts').insert("<li>"+transport.responseJSON.newThought+"</li>");
+			console.debug("response received"+transport.responseJSON.created);
+			var quickThought = new QuickThought(transport.responseJSON);
+			mindDump.show(quickThought);
 		}
 	});
 }
